64576315ba504262ac2567a89dd63e90417568ae
lericke <araujo.lericke@gmail.com>
2021-08-25 07:43:32 -0300

    Aprimorando get e set

--- conta.py
+++ conta.py
@@ -21,3 +21,17 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, novo_limite):
+        self.__limite = novo_limite
--- teste_conta.py
+++
@@ -1,5 +0,0 @@
-
-
-def chama_conta():
-    conta = Conta()
-    print(conta)
